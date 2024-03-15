8f4c829881e22b353b522cd053e6ef0e1ea3b50f
JRMendDemo <122036228+JRMendDemo@users.noreply.github.com>
2024-03-15 15:41:36 -0500

    Update downloadCLI.groovy

--- vars/downloadCLI.groovy
+++ vars/downloadCLI.groovy
@@ -1,4 +1,4 @@
-def downloadMendCLI() { 
+def call() { 
       echo 'Downloading Mend CLI'
       sh 'curl -LJO https://downloads.mend.io/production/unified/latest/linux_amd64/mend && chmod +x mend'
 }
