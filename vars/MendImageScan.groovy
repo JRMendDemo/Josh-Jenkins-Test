9f135ecf835deea7cb6d8facb88d361e7aa4f4e4
JRMendDemo <122036228+JRMendDemo@users.noreply.github.com>
2024-03-18 09:42:12 -0500

    Update MendImageScan.groovy

--- vars/MendImageScan.groovy
+++ vars/MendImageScan.groovy
@@ -1,11 +1,10 @@
 def call() { 
-      echo 'Run Mend dependencies scan'
-      catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
-            sh '''
-            export repo=$(basename -s .git $(git config --get remote.origin.url))
-            export branch=$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -s *//${JOB_NAME}//${repo}_${branch} --fail-policy --non-interactive --export-results dep-results.txt
-            '''
-            archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
-      }
+   echo 'Start Mend Code Scan'
+   catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
+      sh '''
+      export repo=$(basename -s .git $(git config --get remote.origin.url))
+      export branch=$(git rev-parse --abbrev-ref HEAD)
+      ./mend image --non-interactive -s *//${JOB_NAME}//${repo}_${branch}
+      '''
+ }
 }
